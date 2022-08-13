dc3525f2be3e23f5268c7f79869dc79a219c0266
duweiquan <duweiquan@intasect.com.cn>
2022-08-13 17:02:18 +0800

    全文搜索

--- docs/public/js/pgmanor-self.js
+++ docs/public/js/pgmanor-self.js
@@ -1,5 +1,10 @@
 // https://cloud.tencent.com/developer/article/2018247
 // vuepress-plugin-fulltext-search 添加全局配置
-SEARCH_MAX_SUGGESTIONS = 10;
+// SEARCH_MAX_SUGGESTIONS 定义最大搜索结果数据
+// SEARCH_PATHS 搜索路径
+// SEARCH_HOTKEYS 快捷键
+/* eslint-disable */
+SEARCH_MAX_SUGGESTIONS = 100;
 SEARCH_PATHS = [];
 SEARCH_HOTKEYS = ["s", "i"];
+/* eslint-enable */
